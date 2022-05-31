b3461b7d4437e61cf743010bc6c0db3b007b61aa
hnrqblck <hnrqblck@gmail.com>
2022-05-31 11:02:02 -0300

    change axios headers

--- src/services/api.js
+++ src/services/api.js
@@ -4,5 +4,8 @@ export default axios.create({
     baseURL: 'http://144.126.213.60',
     headers: {
         'Content-Type': 'application/json',
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "DELETE, POST, GET, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Requested-With"
     },
 });
